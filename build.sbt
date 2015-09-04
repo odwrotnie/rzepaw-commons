ce9d08a16d7760b77f3db580e5df0f67fb08c245
rzepa <odwrotnie@gmail.com>
2015-09-04 22:53:11 +0200

    identitytest

--- build.sbt
+++ build.sbt
@@ -54,3 +54,5 @@ libraryDependencies += "org.apache.poi" % "poi-scratchpad" % "3.10-FINAL"
 libraryDependencies += "com.typesafe.slick" %% "slick" % "3.0.0"
 
 libraryDependencies += "com.wix" %% "accord-core" % "0.5"
+
+libraryDependencies += "com.h2database" % "h2" % "1.3.175"
--- src/test/scala/slicky/entity/IdEntityTest.scala
+++ src/test/scala/slicky/entity/IdEntityTest.scala
@@ -1,35 +1,42 @@
-//package slicky.entity
-//
-//import commons.logger._
-//import org.scalatest._
-//import slicky.Slicky._
-//import driver.api._
-//
-//// sbt "~rzepawCommons/testOnly slicky.entity.IdEntityTest"
-//class IdEntityTest
-//  extends FunSuite
-//  with Logger {
-//
-//}
-//
-//case class IdName(name: String,
-//                  var id: Option[ID])
-//  extends IdEntity[IdName](NumberNameMeta) {
-//}
-//
-//
-//object NumberNameMeta
-//  extends IdEntityMeta[IdName] {
-//
-//  val table = TableQuery[Tbl]
-//
-//  class Tbl(tag: Tag)
-//    extends Table[IdName](tag, "ID_NAME") {
-//
-//    def name = column[String]("NAME")
-//    def id = column[ID]("ID", O.PrimaryKey, O.AutoInc)
-//
-//    def * = (name, id.?) <>
-//      ((IdName.apply _).tupled, IdName.unapply)
-//  }
-//}
+package slicky.entity
+
+import commons.logger._
+import org.scalatest.FunSuite
+import slicky.Slicky._
+import driver.api._
+
+// sbt "~rzepawCommons/testOnly slicky.entity.IdEntityTest"
+class IdEntityTest
+  extends FunSuite
+  with Logger {
+
+  dbAwait {
+    NumberNameMeta.table.schema.create
+  }
+
+  test("Insert entity") {
+
+  }
+}
+
+case class IdName(name: String,
+                  var id: Option[ID])
+  extends IdEntity[IdName](NumberNameMeta) {
+}
+
+
+object NumberNameMeta
+  extends IdEntityMeta[IdName] {
+
+  val table = TableQuery[Tbl]
+
+  class Tbl(tag: Tag)
+    extends Table[IdName](tag, "ID_NAME") {
+
+    def name = column[String]("NAME")
+    def id = column[ID]("ID", O.PrimaryKey, O.AutoInc)
+
+    def * = (name, id.?) <>
+      ((IdName.apply _).tupled, IdName.unapply)
+  }
+}
