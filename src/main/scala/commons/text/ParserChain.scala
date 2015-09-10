88e56b931f6525117a563e198c502a593a70d07e
rzepa <odwrotnie@gmail.com>
2015-09-10 12:02:10 +0200

    dziala wszystko, nie tak jak u Karoliny

--- src/main/scala/commons/text/ParserChain.scala
+++ src/main/scala/commons/text/ParserChain.scala
@@ -1,9 +1,15 @@
 package commons.text
 
+import commons.logger.Logger
+
 import scala.util.Try
 
-case class ParserChain[T](list: (String => T) *) {
-  def apply(s: String): Option[T] = list.flatMap { st =>
-    Try(st(s)).toOption
-  }.headOption
+case class ParserChain[T](list: (String => T) *)
+  extends Logger {
+  def apply(s: String): Option[T] = {
+    debug(s"Parsing: $s with ${ list.size } steps")
+    list.flatMap { st =>
+      Try(st(s)).toOption
+    }.headOption
+  }
 }
