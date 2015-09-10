79ec527dcf67911d88bcccc41001a22f4f24104d
rzepa <odwrotnie@gmail.com>
2015-09-10 11:55:26 +0200

    temporar data error

--- src/main/scala/commons/text/ParserChain.scala
+++ src/main/scala/commons/text/ParserChain.scala
@@ -3,7 +3,7 @@ package commons.text
 import scala.util.Try
 
 case class ParserChain[T](list: (String => T) *) {
-  def parse(s: String): Option[T] = list.flatMap { st =>
+  def apply(s: String): Option[T] = list.flatMap { st =>
     Try(st(s)).toOption
   }.headOption
 }
