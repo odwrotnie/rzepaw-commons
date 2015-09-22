c44cb7bffc617fcf63694ab3c6aeebf5d242f1a3
rzepa <odwrotnie@gmail.com>
2015-09-22 11:05:39 +0200

    nowe powiadomienia i nowe logo, pizda

--- src/main/scala/commons/logger/GoogleMessaging.scala
+++ src/main/scala/commons/logger/GoogleMessaging.scala
@@ -2,7 +2,7 @@ package commons.logger
 
 import spray.client.pipelining._
 import spray.http.Uri.Path
-import spray.http.{HttpRequest, HttpResponse, Uri}
+import spray.http._
 import spray.httpx.{PipelineException, SprayJsonSupport, UnsuccessfulResponseException}
 
 import akka.actor.ActorSystem
@@ -11,36 +11,37 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 import scala.util.parsing.json.JSONObject
 
-case class GoogleMessaging(key: String)
+case class GoogleMessaging(title: String, key: String)
   extends Logger {
 
   implicit val system = ActorSystem()
   import system.dispatcher
 
-  val URI = Uri("https://gcm-http.googleapis.com")
-  val pipeline = (
-    addHeader("Authorization", s"key=$key")
-      ~> addHeader("Content-Type", "application/json")
-      ~> sendReceive
-      ~> unmarshal[String])
-  val uri = URI.withPath(Path / "gcm" / "send")
-  val json = JSONObject(Map(
-    "notification" -> JSONObject(Map(
-      "sound" -> "default",
-      "badge" -> "1",
-      "title" -> "name",
-      "body" -> "message",
-      "icon" -> "ic_stat_ic_notification"
-    )),
-    "to" -> "/topics/global"
-  ))
-  val post = Post(uri, json.toString())
-  debug(s"Google Messaging POST: $post")
-  val ppl: Future[String] = pipeline(post)
-  //    val futureResult = ppl.map { t: String =>
-  //      info(s"NMG API Token: $t")
-  //      Some(t)
-  //    }
-  val response = Await.result(ppl, Duration.Inf)
-  info("RESPONSE: " + response)
+  def notify(message: String): Unit = {
+    val URI = Uri("https://gcm-http.googleapis.com")
+    val pipeline = (
+      addHeader("Authorization", s"key=$key")
+        ~> sendReceive
+        ~> unmarshal[String])
+    val uri = URI.withPath(Path / "gcm" / "send")
+    val json = JSONObject(Map(
+      "notification" -> JSONObject(Map(
+        "sound" -> "default",
+        "badge" -> "1",
+        "title" -> title,
+        "body" -> message,
+        "icon" -> "ic_stat_ic_notification"
+      )),
+      "to" -> "/topics/global"
+    ))
+    val post = Post(uri, HttpEntity(MediaTypes.`application/json` ,json.toString()))
+    // debug(s"Google Messaging POST: $post")
+    val ppl: Future[String] = pipeline(post)
+    //    val futureResult = ppl.map { t: String =>
+    //      info(s"NMG API Token: $t")
+    //      Some(t)
+    //    }
+    val response = Await.result(ppl, Duration.Inf)
+    info(s"Google messaging response: $response")
+  }
 }
