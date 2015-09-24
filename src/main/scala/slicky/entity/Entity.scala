df2f40e74322825da782f4ef46be6305471be0a9
rzepa <odwrotnie@gmail.com>
2015-09-24 15:32:26 +0200

    infinite scroll

--- src/main/scala/slicky/entity/Entity.scala
+++ src/main/scala/slicky/entity/Entity.scala
@@ -24,9 +24,14 @@ abstract class EntityMeta[E <: Entity[E]] {
     e
   }
 
-  def query: Query[T, E, Seq] = table
   def stream(query: Query[T, E, Seq]): Stream[E] = streamify(query)
-  def stream: Stream[E] = stream(query)
+  def stream: Stream[E] = stream(table)
+  private def streamPage(stream: Stream[E], page: Int, pageSize: Int): Stream[E] =
+    stream.take((page + 1) * pageSize).takeRight(pageSize) // stream.take((page + 1) * pageSize).reverse.take(pageSize).reverse
+  def streamPage(query: Query[T, E, Seq], page: Int, pageSize: Int): Stream[E] =
+    streamPage(stream(query), page, pageSize)
+  def streamPage(page: Int, pageSize: Int): Stream[E] =
+    streamPage(stream, page, pageSize)
 
   // BEFORE
   def beforeInsert(e: E): E = e
