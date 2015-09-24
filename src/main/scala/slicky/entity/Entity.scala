7717cd5f7ae2ba51e46cbeac86daf744a6047f3b
rzepa <odwrotnie@gmail.com>
2015-09-24 16:35:57 +0200

    poprawa

--- src/main/scala/slicky/entity/Entity.scala
+++ src/main/scala/slicky/entity/Entity.scala
@@ -27,7 +27,8 @@ abstract class EntityMeta[E <: Entity[E]] {
   def stream(query: Query[T, E, Seq]): Stream[E] = streamify(query)
   def stream: Stream[E] = stream(table)
   private def streamPage(stream: Stream[E], page: Int, pageSize: Int): Stream[E] =
-    stream.take((page + 1) * pageSize).takeRight(pageSize) // stream.take((page + 1) * pageSize).reverse.take(pageSize).reverse
+    // stream.take((page + 1) * pageSize).takeRight(pageSize)
+    stream.take((page + 1) * pageSize).reverse.take(pageSize).reverse
   def streamPage(query: Query[T, E, Seq], page: Int, pageSize: Int): Stream[E] =
     streamPage(stream(query), page, pageSize)
   def streamPage(page: Int, pageSize: Int): Stream[E] =
