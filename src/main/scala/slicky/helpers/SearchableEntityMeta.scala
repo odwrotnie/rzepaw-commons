c208cd8d09d086c15736a3e54712950c18973527
rzepa <odwrotnie@gmail.com>
2015-09-25 12:53:29 +0200

    queryfix

--- src/main/scala/slicky/helpers/SearchableEntityMeta.scala
+++ src/main/scala/slicky/helpers/SearchableEntityMeta.scala
@@ -1,17 +1,24 @@
 package slicky.helpers
 
+import commons.logger.Logger
 import slicky.entity.{EntityMeta, Entity}
 import slicky.Slicky._
 import driver.api._
 
 import scala.concurrent.Future
 
-trait SearchableEntityMeta[E <: Entity[E]] {
+trait SearchableEntityMeta[E <: Entity[E]]
+  extends Logger {
 
   self: EntityMeta[E] =>
 
   def searchQuery(query: String): Query[T, E, Seq]
 
   def searchPage(query: String, pageNum: Int, pageSize: Int): Future[Seq[E]] =
-    page(searchQuery(query), pageNum, pageSize)
+    if (query.nonEmpty) {
+      page(searchQuery(query), pageNum, pageSize)
+    } else {
+      warn("Search query is empty")
+      page(allQuery, pageNum, pageSize)
+    }
 }
