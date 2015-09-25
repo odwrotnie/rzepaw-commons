8772dfac899634b3a87fd8aadba4bc1262f66d33
rzepa <odwrotnie@gmail.com>
2015-09-25 12:50:12 +0200

    queryfix

--- src/main/scala/slicky/helpers/SearchableEntityMeta.scala
+++ src/main/scala/slicky/helpers/SearchableEntityMeta.scala
@@ -4,11 +4,14 @@ import slicky.entity.{EntityMeta, Entity}
 import slicky.Slicky._
 import driver.api._
 
+import scala.concurrent.Future
+
 trait SearchableEntityMeta[E <: Entity[E]] {
 
   self: EntityMeta[E] =>
 
-  def searchQuery(query: String): Query[_, E, Seq]
+  def searchQuery(query: String): Query[T, E, Seq]
 
-  def searchStream(query: String): Stream[E] = streamify(searchQuery(query))
+  def searchPage(query: String, pageNum: Int, pageSize: Int): Future[Seq[E]] =
+    page(searchQuery(query), pageNum, pageSize)
 }
