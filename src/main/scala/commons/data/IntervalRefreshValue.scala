31760def84abae244602c068ea6715692827b23c
rzepa <odwrotnie@gmail.com>
2015-09-26 20:27:25 +0200

    nmg

--- src/main/scala/commons/data/IntervalRefreshValue.scala
+++ src/main/scala/commons/data/IntervalRefreshValue.scala
@@ -2,6 +2,7 @@ package commons.data
 
 import commons.date.{DateInterval, Day, DateUtil}
 import commons.logger.Logger
+import commons.statistics.Timed
 import org.joda.time.DateTime
 
 class IntervalRefreshValue[T](retrieve: => T, intervalHours: Float = 3f)
@@ -13,9 +14,12 @@ class IntervalRefreshValue[T](retrieve: => T, intervalHours: Float = 3f)
   def get: T = if ((DateUtil.now.getMillis - lastTimeRefreshed) < (intervalHours * DateUtil.MILLISECONDS_IN_HOUR)) {
     value
   } else {
-    val r = (retrieve, DateUtil.now.getMillis)
+    val timed = Timed("Retrieve")
+    val r = timed.log((retrieve, DateUtil.now.getMillis))
     value = r._1
     lastTimeRefreshed = r._2
     value
   }
+
+  override def toString = get.toString
 }
--- src/main/scala/commons/statistics/Timed.scala
+++ src/main/scala/commons/statistics/Timed.scala
@@ -7,20 +7,25 @@ case class Timed(name: String)
   extends Logger {
   var s = System.nanoTime
   var e = System.nanoTime
+
   def time = e - s
+
   def start { s = System.nanoTime }
   def end { e = System.nanoTime }
+
   def measure[T](thunk: => T) = {
     start
     val ret = thunk
     end
     ret
   }
+
   def log[T](thunk: => T) = {
     val ret = measure(thunk)
     debug(this.toString)
     ret
   }
+
   override def toString = "[%s] Executed in %s"
     .format(name, HumanReadable.nanoseconds(time))
 }
