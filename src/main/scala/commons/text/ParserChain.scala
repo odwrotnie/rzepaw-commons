ca06b8e0b57395f5d9a36b8bfc45920b4cc88c30
rzepa <odwrotnie@gmail.com>
2015-09-27 22:26:13 +0200

    ig

--- src/main/scala/commons/text/ParserChain.scala
+++ src/main/scala/commons/text/ParserChain.scala
@@ -7,11 +7,10 @@ import scala.util.Try
 case class ParserChain[T](list: (String => T) *)
   extends Logger {
 
+  // TODO Przekazywać jako funkcje i obliczać do pierwszej poprawnej
   def apply(s: String): Option[T] = {
     val steps = list.map { st =>
-      Try {
-        st(s)
-      }.toOption
+      Try(st(s)).toOption
     }
     debug(s"Parsing: $s with ${ list.size } steps - ${ steps.mkString("[ ", " | ", " ]") }")
     steps.flatten.headOption
