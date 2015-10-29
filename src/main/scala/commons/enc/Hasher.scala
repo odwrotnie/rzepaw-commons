f98ffbba9b2b88f7da7dc2fa76cce0edef9220bd
rzepa <odwrotnie@gmail.com>
2015-10-29 20:25:19 +0100

    filip chuj

--- src/main/scala/commons/enc/Hasher.scala
+++ src/main/scala/commons/enc/Hasher.scala
@@ -1,15 +1,18 @@
 package commons.enc
 
-// import com.roundeights.hasher.Implicits._
+// TODO https://github.com/Nycto/Hasher
+
 import scala.language.postfixOps
 
 object Hasher {
 
+  // TODO https://github.com/Nycto/Hasher
   def encrypt(word: String): Array[Byte] = {
     // word.md5.bytes
     Array[Byte]()
   }
 
+  // TODO https://github.com/Nycto/Hasher
   def equal(hashed: Array[Byte], word: String): Boolean = {
     // word.md5 hash= hashed
     true
