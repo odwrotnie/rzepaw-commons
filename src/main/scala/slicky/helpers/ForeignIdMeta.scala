1a1d89349d94a3bc885287be795211dd88529686
rzepa <odwrotnie@gmail.com>
2015-10-29 11:18:49 +0100

    duzo zmian

--- src/main/scala/slicky/helpers/ForeignIdMeta.scala
+++ src/main/scala/slicky/helpers/ForeignIdMeta.scala
@@ -8,9 +8,7 @@ import scala.concurrent.Future
 trait ForeignIdMeta[E <: Entity[E]] {
   type FIT = Table[E] { def foreignId: Rep[Option[String]] }
   def table: TableQuery[_ <: FIT]
-  def byForeignId(foreignId: Option[String]): Future[Seq[E]] = dbFuture {
-    MaybeFilter(table)
-      .filter(foreignId)(f => row => row.foreignId === f)
-      .query.result
+  def byForeignId(foreignId: String): Stream[E] = streamify {
+    table.filter(_.foreignId === foreignId)
   }
 }
