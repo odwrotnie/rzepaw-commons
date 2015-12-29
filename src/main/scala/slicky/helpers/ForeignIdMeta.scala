de38f49c34836950c1c2c5a0b8eeb0e1bfdfd1cb
rzepa <odwrotnie@gmail.com>
2015-12-29 12:40:35 +0100

    foreign key meta

--- src/main/scala/slicky/helpers/ForeignIdMeta.scala
+++ src/main/scala/slicky/helpers/ForeignIdMeta.scala
@@ -2,13 +2,38 @@ package slicky.helpers
 
 import slicky.Slicky._
 import driver.api._
-import slicky.entity.Entity
+import slicky.entity.{EntityMeta, Entity}
 import scala.concurrent.Future
 
-trait ForeignIdMeta[E <: Entity[E]] {
-  type FIT = Table[E] { def foreignId: Rep[Option[String]] }
+trait ForeignIdEntityMeta[FIE <: Entity[FIE] {def foreignId: Option[String]; def withForeignId(foreignId: String): FIE}] {
+
+  type FIT = Table[FIE] { def foreignId: Rep[Option[String]] }
   def table: TableQuery[_ <: FIT]
-  def byForeignId(foreignId: String): Stream[E] = streamify {
+
+  def byForeignIdQuery(foreignId: String): Query[FIT, FIE, Seq] =
     table.filter(_.foreignId === foreignId)
+
+  def byForeignId(foreignId: String): Stream[FIE] = streamify(byForeignIdQuery(foreignId))
+
+  def updateByForeignId(entity: FIE): DBIO[Int] = {
+    require(entity.foreignId.nonEmpty)
+    val foreignId = entity.foreignId.get
+    byForeignIdQuery(foreignId).update(entity)
+  }
+  def updateByForeignId(foreignId: String, entity: FIE): DBIO[Int] = {
+    val e = entity.withForeignId(foreignId)
+    updateByForeignId(e)
+  }
+
+  def insert(e: FIE): DBIO[FIE]
+  def updateOrInsertByForeignId(entity: FIE): DBIO[FIE] = {
+    updateByForeignId(entity) flatMap {
+      case rows if rows == 0 => insert(entity)
+      case _ => DBIO.successful(entity)
+    }
+  }
+  def updateOrInsertByForeignId(foreignId: String, entity: FIE): DBIO[FIE] = {
+    val e = entity.withForeignId(foreignId)
+    updateOrInsertByForeignId(e)
   }
 }
