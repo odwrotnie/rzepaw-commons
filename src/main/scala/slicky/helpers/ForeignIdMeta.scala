52857da5825920381f72d7b5f7fcfd8239969163
rzepa <odwrotnie@gmail.com>
2016-01-04 14:26:48 +0100

    update or insert by foreign id

--- src/main/scala/slicky/helpers/ForeignIdMeta.scala
+++ src/main/scala/slicky/helpers/ForeignIdMeta.scala
@@ -15,12 +15,15 @@ trait ForeignIdEntityMeta[FIE <: Entity[FIE] {def foreignId: Option[String]; def
 
   def byForeignId(foreignId: String): Stream[FIE] = streamify(byForeignIdQuery(foreignId))
 
-  def updateByForeignId(entity: FIE): DBIO[Int] = {
+  def updateByForeignId(entity: FIE): DBIO[Option[FIE]] = {
     require(entity.foreignId.nonEmpty)
     val foreignId = entity.foreignId.get
-    byForeignIdQuery(foreignId).update(entity)
+    byForeignIdQuery(foreignId).update(entity) map {
+      case rows if rows == 0 => None
+      case rows if rows == 1 => byForeignId(foreignId).headOption
+    }
   }
-  def updateByForeignId(foreignId: String, entity: FIE): DBIO[Int] = {
+  def updateByForeignId(foreignId: String, entity: FIE): DBIO[Option[FIE]] = {
     val e = entity.withForeignId(foreignId)
     updateByForeignId(e)
   }
@@ -28,8 +31,8 @@ trait ForeignIdEntityMeta[FIE <: Entity[FIE] {def foreignId: Option[String]; def
   def insert(e: FIE): DBIO[FIE]
   def updateOrInsertByForeignId(entity: FIE): DBIO[FIE] = {
     updateByForeignId(entity) flatMap {
-      case rows if rows == 0 => insert(entity)
-      case _ => DBIO.successful(entity)
+      case Some(e) => DBIO.successful(e)
+      case None => insert(entity)
     }
   }
   def updateOrInsertByForeignId(foreignId: String, entity: FIE): DBIO[FIE] = {
