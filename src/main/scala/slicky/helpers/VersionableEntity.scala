d183c68437e7058a2ba1d7eef09cda7cc296363e
rzepa <odwrotnie@gmail.com>
2016-01-31 19:21:57 +0100

    linked guard

--- src/main/scala/slicky/helpers/VersionableEntity.scala
+++ src/main/scala/slicky/helpers/VersionableEntity.scala
@@ -5,9 +5,12 @@ import slicky.entity.IdEntity
 
 import scala.concurrent.Future
 import slicky.Slicky._
+import driver.api._
 
 trait VersionableEntity[E <: IdEntity[E] with CreatedableEntity]
   extends CreatedableEntity { self: IdEntity[E] with CreatedableEntity =>
+
+  //def meta: VersionableEntityMeta[E]
 
   def previousVersionId: Option[ID]
   //def previousVersion: Option[E] = previousVersionId.map(id => meta.byIdentGet(id).future)
@@ -19,3 +22,8 @@ trait VersionableEntity[E <: IdEntity[E] with CreatedableEntity]
 //
 //  meta.table.filter(_.pre)
 }
+
+//trait VersionableEntityMeta[E <: VersionableEntity[E]] {
+//  type VT = Table[E] { def previousVersionId: Rep[Option[ID]] }
+//  def table: TableQuery[_ <: VT]
+//}
