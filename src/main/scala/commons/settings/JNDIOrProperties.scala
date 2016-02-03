bf863edd44404b91f6b9f547d8dddd0d0a559490
rzepa <odwrotnie@gmail.com>
2016-02-03 22:49:54 +0100

    log

--- src/main/scala/commons/settings/JNDIOrProperties.scala
+++ src/main/scala/commons/settings/JNDIOrProperties.scala
@@ -1,15 +1,20 @@
 package commons.settings
 
+import commons.logger.Logger
+
 import scala.util.Try
 
-object JNDIOrProperties {
+object JNDIOrProperties
+  extends Logger {
 
   def get(path: String*): Option[String] = {
     val results: List[String] = List(
       JNDI.get(path.mkString("/")),
       ResourceProperties(s"/${ path.head }.properties").get(path.tail.mkString("."))
     ).flatten
-    results.headOption
+    val result = results.headOption
+    debug(s"JNDI or properties lookup: ${ path.mkString("/") } = $result")
+    result
   }
 
   def getInt(path: String*): Option[Int] = get(path:_*).flatMap(s => Try(s.toInt).toOption)
