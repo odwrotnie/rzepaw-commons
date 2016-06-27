6e0a86806f2970202a20d0c66edbc9a633c2a1e3
rzepa <odwrotnie@gmail.com>
2016-06-27 11:31:16 +0200

    mmap

--- src/main/scala/commons/data/MultiMap.scala
+++ src/main/scala/commons/data/MultiMap.scala
@@ -1,7 +1,10 @@
 package commons.data
 
-case class MultiMap[T, V](m: collection.mutable.Map[T, Either[MultiMap[T, V], V]],
-                       parent: Option[MultiMap[T, V]] = None) {
+import collection.mutable._
+
+case class MultiMap[T, V](value: Option[V],
+                          m: Map[T, MultiMap[T, V]],
+                          parent: Option[MultiMap[T, V]] = None) {
 
   private def level: Int = parent match {
     case None => 0
@@ -14,33 +17,31 @@ case class MultiMap[T, V](m: collection.mutable.Map[T, Either[MultiMap[T, V], V]
     case t :: Nil =>
       m.get(t) match {
         case None =>
-          m += t -> Right(v)
+          m += t -> MultiMap(Some(v), Map[T, MultiMap[T, V]](), Some(this))
         case Some(_) =>
           throw new Exception(s"Key `$t` already exists")
       }
     case t :: tail =>
       m.get(t) match {
         case None =>
-          val mmm = MultiMap(collection.mutable.Map[T, Either[MultiMap[T, V], V]](), Some(this))
-          m += t -> Left(mmm)
-          mmm.insert(tail:_*)(v)
-        case Some(Left(mmm)) =>
-          mmm.insert(tail:_*)(v)
-        case Some(Right(v)) =>
-          throw new Exception(s"Value `$v` already exists")
+          val mm = MultiMap(None, Map[T, MultiMap[T, V]](), Some(this))
+          m += t -> mm
+          mm.insert(tail:_*)(v)
+        case Some(mm) =>
+          mm.insert(tail:_*)(v)
       }
   }
 
-  override def toString = m.map {
-    case (t, Left(m)) => s"+ ${ "  " * level }$t:\n$m"
-    case (t, Right(a)) => s"- ${ "  " * level }$t: $a"
-  } mkString "\n"
+  override def toString = s"${ value.getOrElse("") }\n" + (m.map {
+    case (t, mm) => s"${ "  " * level }$t: $mm"
+  } mkString "")
 }
 
 object MultiMap {
-  def empty[T, V](): MultiMap[T, V] = MultiMap[T, V](collection.mutable.Map[T, Either[MultiMap[T, V], V]]())
+  def empty[T, V](): MultiMap[T, V] = MultiMap[T, V](None, Map[T, MultiMap[T, V]]())
+  def apply[T, V](value: V): MultiMap[T, V] = apply(Some(value), Map[T, MultiMap[T, V]]())
   def apply[T, V](tuples: Tuple2[T, V]*): MultiMap[T, V] = {
-    val mmmap = MultiMap[T, V](collection.mutable.Map[T, Either[MultiMap[T, V], V]]())
+    val mmmap = MultiMap[T, V](None, Map[T, MultiMap[T, V]]())
     tuples foreach {
       case (k, v) => mmmap.insert(k)(v)
     }
--- src/test/scala/commons/data/MultiMapTest.scala
+++ src/test/scala/commons/data/MultiMapTest.scala
@@ -11,8 +11,8 @@ class MultiMapTest
 
   "MultiMap" should "work" in {
     val mm = MultiMap.empty[String, String]()
-    mm.insert("person", "name")("John")
-    mm.insert("person", "company")("Subeli")
+    mm.insert("person", "name")("John")§
+    mm.insert("person", "company", "name")("Subeli")
     mm.insert("person", "days", "1", "start-hour")("1")
     mm.insert("person", "days", "1", "end-hour")("1")
     mm.insert("person", "days", "1", "duration")("1")
