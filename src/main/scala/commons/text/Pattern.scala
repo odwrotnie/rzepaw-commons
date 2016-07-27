4be6b6b03c949aaa29e532ea9940ad8df50da851
rzepa <odwrotnie@gmail.com>
2016-07-27 09:00:38 +0200

    pattern

--- src/main/scala/commons/text/Pattern.scala
+++ src/main/scala/commons/text/Pattern.scala
@@ -5,9 +5,11 @@ import scala.util.matching.Regex
 
 object Pattern {
 
-  lazy val INTEGER_PATTERN = "\\d+".r
-  lazy val DOUBLE_PATTERN = "\\d+([\\.,]\\d+)?".r
-  lazy val EMAIL_PATTERN = """([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+""".r
+  lazy val INTEGER_REGEX = "\\d+".r
+  lazy val DOUBLE_REGEX = "\\d+([\\.,]\\d+)?".r
+
+  lazy val EMAIL_PATTERN = """([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+"""
+  lazy val EMAIL_REGEX = EMAIL_PATTERN.r
 
   // FIRST
 
@@ -20,13 +22,13 @@ object Pattern {
     pickFirst(s.r)(text)
 
   def pickFirstInteger(text: String): Option[Int] =
-    Try(pickFirst(INTEGER_PATTERN)(text).get.toInt).toOption
+    Try(pickFirst(INTEGER_REGEX)(text).get.toInt).toOption
 
   def pickFirstDouble(text: String): Option[Double] =
-    Try(pickFirst(DOUBLE_PATTERN)(text).get.replace(",", ".").toDouble).toOption
+    Try(pickFirst(DOUBLE_REGEX)(text).get.replace(",", ".").toDouble).toOption
 
   def pickFirstEmail(text: String): Option[String] =
-    pickFirst(EMAIL_PATTERN)(text)
+    pickFirst(EMAIL_REGEX)(text)
 
   // ALL
 
@@ -34,14 +36,14 @@ object Pattern {
     pattern.findAllIn(text).toList
 
   def pickAllIntegers(text: String): List[Int] =
-    pickAll(INTEGER_PATTERN)(text).map(_.toInt)
+    pickAll(INTEGER_REGEX)(text).map(_.toInt)
 
   def pickIntegers(text: String): Option[String] =
-    pickAll(INTEGER_PATTERN)(text).mkString match {
+    pickAll(INTEGER_REGEX)(text).mkString match {
       case s: String if s.nonEmpty => Some(s)
       case _ => None
     }
 
   def pickAllEmails(text: String): List[String] =
-    pickAll(EMAIL_PATTERN)(text)
+    pickAll(EMAIL_REGEX)(text)
 }
