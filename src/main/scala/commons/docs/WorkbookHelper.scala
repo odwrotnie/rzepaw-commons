31727a30d2b8aa775e970226d2069c4066c3d99a
rzepa <odwrotnie@gmail.com>
2016-09-02 13:18:00 +0200

    data

--- src/main/scala/commons/docs/WorkbookHelper.scala
+++ src/main/scala/commons/docs/WorkbookHelper.scala
@@ -2,6 +2,7 @@ package commons.docs
 
 import java.io.{InputStream, File}
 
+import commons.logger.Logger
 import org.apache.poi.ss.usermodel.{Row, Sheet, Workbook}
 
 import scala.util.Try
@@ -10,7 +11,8 @@ case class WorkbookHelper(wb: Option[Workbook] = None,
                           file: Option[File] = None,
                           input: Option[InputStream] = None,
                           path: Option[String] = None,
-                          dropFirstRows: Int = 0) {
+                          dropFirstRows: Int = 0)
+  extends Logger {
 
   require(List(wb, file, input, path).flatten.nonEmpty)
 
@@ -18,7 +20,12 @@ case class WorkbookHelper(wb: Option[Workbook] = None,
     List(wb, file.map(Excel.workbook), input.map(Excel.workbook), path.map(Excel.workbook))
       .flatten.headOption.get
 
-  val sheets: Stream[Sheet] = (0 until workbook.getNumberOfSheets).map(i => workbook.getSheetAt(i)).toStream
+  val sheets: Stream[Sheet] = (0 until workbook.getNumberOfSheets).map { sheetIndex =>
+    val sheet = workbook.getSheetAt(sheetIndex)
+    debug(s"Loading sheet: ${ sheet.getSheetName }")
+    sheet
+  }.toStream
+
   val sheetHelpers: Stream[SheetHelper] = sheets.map(s => SheetHelper.apply(s, dropFirstRows))
   val rows: Stream[Row] = sheetHelpers.flatMap(_.rows)
   val rowHelpers: Stream[RowHelper] = rows.map(RowHelper.apply)
