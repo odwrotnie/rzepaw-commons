707752d586d4f7386b3cc8df60d9c7b0ffdfe2ef
rzepa <odwrotnie@gmail.com>
2016-10-19 16:17:46 +0200

    copy

--- src/main/scala/slicky/helpers/ForeignIdMeta.scala
+++ src/main/scala/slicky/helpers/ForeignIdMeta.scala
@@ -4,10 +4,6 @@ import slicky.Slicky._
 import driver.api._
 import slicky.entity._
 
-trait ForeignIdEntity {
-
-}
-
 trait ForeignIdEntityMeta[FIE <: Entity[FIE] {def foreignId: Option[String]; def withForeignId(foreignId: String): FIE}] {
 
   type FIT = Table[FIE] { def foreignId: Rep[Option[String]] }
