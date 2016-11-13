60a3fc119c8311a44c6717fff248e982a4612037
rzepa <odwrotnie@gmail.com>
2016-11-13 16:02:19 +0100

    email

--- src/main/scala/commons/files/Files.scala
+++ src/main/scala/commons/files/Files.scala
@@ -3,13 +3,21 @@ package commons.files
 import java.io.{File, FileWriter}
 import java.net.URL
 
+import scala.util.Try
+
 object Files {
+
   def listFromResources(directoryInResources: String): List[File] = {
     val dirUrl: URL = getClass.getResource(directoryInResources)
     val dir = new File(dirUrl.toURI)
     require(dir.isDirectory)
     dir.listFiles().toList
   }
+
+  def textFromResources(path: String*): Option[String] = Try {
+    val is = getClass.getResourceAsStream(path.mkString("/", "/", ""))
+    scala.io.Source.fromInputStream(is).mkString
+  }.toOption
 }
 
 case class TextFile(path: String) {
