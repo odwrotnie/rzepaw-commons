fe1a77e72fb4ed93386942bb9cb6bff9edde606e
rzepa <odwrotnie@gmail.com>
2016-12-11 16:45:06 +0100

    desc stats

--- src/main/scala/commons/data/DescStats.scala
+++ src/main/scala/commons/data/DescStats.scala
@@ -6,12 +6,25 @@ case class DescStats(values: Iterable[Double]) {
 
   lazy val statistics = new DescriptiveStatistics(values.toArray)
 
-  lazy val mean = statistics.getMean
-  lazy val sum = statistics.getSum
-  lazy val stdDev = statistics.getStandardDeviation
-  lazy val variance = statistics.getPopulationVariance
-  lazy val min = statistics.getMin
-  lazy val max = statistics.getMax
+  lazy val mean: Double = statistics.getMean
+  lazy val sum: Double = statistics.getSum
+  lazy val stdDev: Double = statistics.getStandardDeviation
+  lazy val variance: Double = statistics.getPopulationVariance
+  lazy val min: Double = statistics.getMin
+  lazy val max: Double = statistics.getMax
+
+  lazy val kurtosis: Double = statistics.getKurtosis
+  lazy val skewness: Double = statistics.getSkewness
+
+  lazy val q1: Double = statistics.getPercentile(25)
+  lazy val q2: Double = statistics.getPercentile(50)
+  lazy val q3: Double = statistics.getPercentile(75)
+  lazy val iqr: Double = q3 - q1 // interquartile range
+
+  // For box plot - https://en.wikipedia.org/wiki/Box_plot
+  lazy val whiskerLow: Double = q1 - 1.5 * iqr
+  lazy val whiskerHigh: Double = q3 + 1.5 * iqr
+  lazy val outliers: Iterable[Double] = values.filter(_ < whiskerLow).filter(_ > whiskerHigh)
 
   lazy val deciles: Map[Int, Double] = (1 to 9) map { i => i -> statistics.getPercentile(10 * i) } toMap
   lazy val decilesString: String =  (1 to 9) map { i => s"$i: ${statistics.getPercentile(10 * i)}" } mkString ", "
